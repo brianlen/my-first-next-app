106cfc8022850dda5641810f733259080519ebc2
Henry Cho <henry.cho@spaceforce.mil>
2026-02-15 23:30:34 -0700

    Add trailingSlash is true

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: 'export', // This enables static export
   basePath: '/my-first-next-app', // Replace with your GitHub repo name
+  trailingSlash: true,
 };
 
 export default nextConfig;
