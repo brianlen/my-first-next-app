8416722b482df487f535dcf86e1d22cb64ca6214
Henry Cho <henry.cho@spaceforce.mil>
2026-02-17 23:19:54 -0700

    Add assetPrefix

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: 'export', // This enables static export
   basePath: '/my-first-next-app', // Replace with your GitHub repo name
+  assetPrefix: "/my-first-next-app/",
   trailingSlash: true, // for better GitHub Pages compatibility
 };
 
