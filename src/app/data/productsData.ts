45057db87a3fa218bda331f781207f270e5b983a
Henry Cho <henry.cho@spaceforce.mil>
2026-02-21 15:50:50 -0700

    Move EPIKA with Sawyer Squeeze

--- src/app/data/productsData.ts
+++ src/app/data/productsData.ts
@@ -25,20 +25,20 @@ export const productsArray = [
         link: "https://a.co/d/01OVizkm"
     },
     {
-        name: "Sawyer Squeeze",
-        description: "An ultralight 2 oz water filter that lets hikers safely drink from streams or lakes by removing bacteria, protozoa, and microplastics, and easily connects to bottles, pouches, or hydration packs for clean water anywhere on the trail.",
-        price: "$45.95",
-        category: "Water Filter",
-        image: "https://m.media-amazon.com/images/I/71PlK2Jo5uL._AC_SL1500_.jpg",
-        link: "https://a.co/d/08oWDfEH"
-    },
-    {
         name: "EPICKA",
         description: "This compact all-in-one travel adapter works in 200+ countries with 4 plug types and 5 USB ports, letting you safely charge multiple devices at once anywhere in the world.",
         price: "$19.99",
         category: "Universal Travel Adapter",
         image: "https://m.media-amazon.com/images/I/71Z8QXcvteL._AC_SL1500_.jpg",
         link: "https://a.co/d/0gyFIfh9"
+    },
+    {
+        name: "Sawyer Squeeze",
+        description: "An ultralight 2 oz water filter that lets hikers safely drink from streams or lakes by removing bacteria, protozoa, and microplastics, and easily connects to bottles, pouches, or hydration packs for clean water anywhere on the trail.",
+        price: "$45.95",
+        category: "Water Filter",
+        image: "https://m.media-amazon.com/images/I/71PlK2Jo5uL._AC_SL1500_.jpg",
+        link: "https://a.co/d/08oWDfEH"
     },
     {
         name: "Smartwool Everyday",
