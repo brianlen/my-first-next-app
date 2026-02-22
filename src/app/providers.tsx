8b888fed678e89121887065cebafa19ff1a8ec8f
Henry Cho <henry.cho@spaceforce.mil>
2026-02-21 17:37:15 -0700

    Formatting

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -5,22 +5,22 @@ import CssBaseline from '@mui/material/CssBaseline';
 import { ReactNode } from 'react';
 
 const theme = createTheme({
-    palette: {
-      primary: {
-        main: '#1976d2', // customizable primary color
-      },
-      secondary: {
-        main: '#dc004e', // customizable secondary color
-      },
-      background: {
-        default: '#000000', // customize the default background color
-        paper: '#ffffff', // customize the background color of paper-like components
-      },
+  palette: {
+    primary: {
+      main: '#1976d2', // customizable primary color
     },
-    typography: {
-      fontFamily: 'Roboto, Arial, sans-serif',
+    secondary: {
+      main: '#dc004e', // customizable secondary color
     },
-  });
+    background: {
+      default: '#000000', // customize the default background color
+      paper: '#ffffff', // customize the background color of paper-like components
+    },
+  },
+  typography: {
+    fontFamily: 'Roboto, Arial, sans-serif',
+  },
+});
 
 export default function Providers({ children }: { children: ReactNode }) {
   return (
